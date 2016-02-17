6abf00be5fb87a28807de73d0961ac1dd7753b6a
Daniel Pecos Martinez <contact@danielpecos.com>
2016-02-17 15:19:21 +0100

    Parallel stream example

--- src/main/java/com/danielpecos/java8/streams/Stream_02_parallel.java
+++ src/main/java/com/danielpecos/java8/streams/Stream_02_parallel.java
@@ -1,4 +1,33 @@
 package com.danielpecos.java8.streams;
 
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.range;
+
 public class Stream_02_parallel {
+
+    public static final int NUMBERS_TO_PROCESS = 1_000_000_000;
+
+    public static void main(String[] args) {
+        IntStream ints1 = range(0, Integer.MAX_VALUE);
+        IntStream ints2 = range(0, Integer.MAX_VALUE);
+
+        IntPredicate isEven = (i) -> {
+            return i % 2 == 0;
+        };
+
+        long start = System.currentTimeMillis();
+
+        ints1.limit(NUMBERS_TO_PROCESS).filter(isEven).reduce(0, (left, right) -> left + right);
+
+        long endSeq = System.currentTimeMillis();
+
+        ints2.parallel().limit(NUMBERS_TO_PROCESS).filter(isEven).reduce(0, (left, right) -> left + right);
+
+        long endPar = System.currentTimeMillis();
+
+        System.out.println("Sequential -> " + (endSeq - start));
+        System.out.println("Parallel -> " + (endPar - endSeq));
+    }
 }
