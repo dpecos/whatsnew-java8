078a76becc41a6e1a31e3c55125cf64c4cb1fed7
Daniel Pecos <me@danielpecos.com>
2016-02-25 10:22:35 +0100

    Empty examples removed

--- src/main/java/com/danielpecos/java8/other/preJava8.java
+++
@@ -1,6 +0,0 @@
-package com.danielpecos.java8.other;
-
-public class preJava8 {
-    // try-with-resources
-
-}
--- src/main/java/com/danielpecos/java8/streams/Stream_04_fp.java
+++
@@ -1,9 +0,0 @@
-package com.danielpecos.java8.streams;
-
-public class Stream_04_fp {
-
-    // mapreduce
-
-    // flatmap
-
-}
